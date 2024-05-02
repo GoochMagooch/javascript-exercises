1b941856916cb5f133b9006b2533b8f449a317c5
Magooch <crvallecito@gmail.com>
2024-05-01 22:22:57 -0600

    Remove comment

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -4,4 +4,3 @@ const repeatString = function() {
 
 // Do not edit below this line
 module.exports = repeatString;
-// another comment to test if vscode terminal will update github, without Ubuntu terminal open (in windows)
