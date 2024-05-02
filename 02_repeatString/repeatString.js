37a3ddc2e3b94e2f0eb9ac05bff3ac95d76d1d0e
Magooch <crvallecito@gmail.com>
2024-05-01 22:05:26 -0600

    Update this file from vscode terminal, without windows Ubuntu terminal open

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -4,3 +4,4 @@ const repeatString = function() {
 
 // Do not edit below this line
 module.exports = repeatString;
+// another comment to test if vscode terminal will update github, without Ubuntu terminal open (in windows)
